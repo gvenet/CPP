a1e7dad356ff1c520a347e52bb72e216df88101b
Guillaume Venet <gvenet@z3r6p3.42lyon.fr>
2021-06-02 17:46:36 +0200

    1

---
+++ Module_03/template/inc/Human.hpp
@@ -0,0 +1,19 @@
+#ifndef HUMAN_HPP
+# define HUMAN_HPP
+
+# include <iostream>
+# include <string>
+# include "Brain.hpp"
+
+class Human
+{
+private:
+
+public:
+
+	Human();
+	~Human();
+
+};
+
+#endif
