b6c2ed93be4b94ebbaf53f26318cb04d80b12268
Guillaume Venet <gvenet@z3r6p3.42lyon.fr>
2021-06-18 18:54:01 +0200

    1

--- Module_07/ex00/inc/whatever.hpp
+++ Module_07/ex00/inc/whatever.hpp
@@ -1,6 +1,16 @@
 #ifndef WHATEVER_HPP
 #define WHATEVER_HPP
+#include <iostream>
 
+template<typename T>
+void swap(T &a, T &b)
+{
+	std::cout << "x : "<< a << " | y : " << b << std::endl;
+	T tmp = a;
+	a = b;
+	b = tmp;
+	std::cout << "x : "<< a << " | y : " << b << std::endl;
+}
 
 
 #endif
