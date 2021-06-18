b7393eff0abd6627d4e5957ba5ca992a47c00753
Guillaume Venet <gvenet@z3r6p3.42lyon.fr>
2021-06-18 09:05:42 +0200

    1

--- Module_06/ex00/inc/convert.hpp
+++ Module_06/ex00/inc/convert.hpp
@@ -4,15 +4,18 @@
 #include <string>
 #include <cstdlib>
 #include <cctype>
+#include <exception>
 
 class Convert
 {
 private:
-	float const		_input;
-	// float const		_fVal;
-	// int const		_iVal;
-	// double const	_dVal;
-	// char const		_cVal;
+	float const _input;
+	float _fVal;
+	int _iVal;
+	double _dVal;
+	char _cVal;
+	int _precisionFlag;
+	int _cFlag;
 	Convert();
 
 public:
@@ -21,8 +24,37 @@ public:
 	Convert &operator=(Convert const &op);
 	virtual ~Convert();
 
+	void fillVar(void);
 	float getF(void) const;
+	int getI(void) const;
+	double getD(void) const;
+	char getC(void) const;
+	int getCFlag(void) const;
+	int getPrecisionFlag(void) const;
 
+	enum precisionFlag
+	{
+		PRECISIONINACTIV,
+		PRECISIONACTIV,
+	};
+
+	enum charDisplayFlag
+	{
+		DISPLAYABLE,
+		NONDISPLAYABLE,
+		IMPOSSIBLE
+	};
+
+	class ExceptionMsg : public std::exception
+	{
+	private:
+		std::string _msg;
+
+	public:
+		ExceptionMsg(std::string const &msg) : _msg(msg) {}
+		virtual ~ExceptionMsg() throw() {}
+		virtual const char *what() const throw() { return _msg.c_str(); }
+	};
 };
 
 std::ostream &operator<<(std::ostream &os, Convert const &rhs);
--- Module_06/ex00/srcs/convert.cpp
+++ Module_06/ex00/srcs/convert.cpp
@@ -1,15 +1,15 @@
 #include "../inc/convert.hpp"
 
-Convert::Convert(char const *cc) : _input(std::strtof(cc, NULL))
+Convert::Convert(char const *cc) : _input(std::strtof(cc, NULL)), _precisionFlag(PRECISIONINACTIV), _cFlag(DISPLAYABLE)
 {
 	std::string str = cc;
-	for (unsigned long i = 0; i < str.length(); i++)
+	if (str.length() > 1)
 	{
-		if (std::isdigit(str.at(i)) == 0)
-		{
-			std::cout << "coucou\n" ;
-		}
+		for (unsigned long i = 0; i < str.length(); i++)
+			if (!std::isdigit(str.at(i)) && str.at(i) != '.')
+				throw ExceptionMsg("Type unknown");
 	}
+	this->fillVar();
 }
 
 Convert::Convert(Convert const &cpy) : _input(cpy._input)
@@ -25,8 +25,63 @@ float Convert::getF(void) const
 	return (this->_input);
 }
 
+int Convert::getI(void) const
+{
+	return (this->_iVal);
+}
+double Convert::getD(void) const
+{
+	return (this->_dVal);
+}
+char Convert::getC(void) const
+{
+	return (this->_cVal);
+}
+
+int Convert::getCFlag(void) const
+{
+	return (this->_cFlag);
+}
+
+int Convert::getPrecisionFlag(void) const
+{
+	return (this->_precisionFlag);
+}
+
+void Convert::fillVar(void)
+{
+
+	this->_fVal = _input;
+	this->_iVal = static_cast<int>(this->_fVal);
+	this->_dVal = static_cast<double>(this->_fVal);
+	if (std::isprint(this->_iVal))
+		this->_cVal = static_cast<char>(this->_iVal);
+	else
+		this->_cFlag = NONDISPLAYABLE;
+	if (this->_fVal / this->_iVal != 1)
+		this->_precisionFlag = PRECISIONACTIV;
+}
+
 std::ostream &operator<<(std::ostream &os, Convert const &rhs)
 {
-	os << rhs.getF();
+
+	os << "c : ";
+	if (rhs.getCFlag() == Convert::NONDISPLAYABLE)
+		os << "Non displayable" << std::endl;
+	else if (rhs.getCFlag() == Convert::IMPOSSIBLE)
+		os << "Impossible" << std::endl;
+	else
+		os << rhs.getC() << std::endl;
+
+	os << "i : " << rhs.getI() << std::endl;
+
+	// os << "TEST : " << rhs.getPrecisionFlag() << std::endl;
+
+	if (rhs.getPrecisionFlag() == Convert::PRECISIONINACTIV || rhs.getF() == 0)
+		os << "f : " << rhs.getF() << ".0f" << std::endl
+		   << "d : " << rhs.getD() << ".0" << std::endl;
+	else
+		os << "f : " << rhs.getF() << "f" <<std::endl
+		   << "d : " << rhs.getD() << std::endl;
 	return os;
 }
