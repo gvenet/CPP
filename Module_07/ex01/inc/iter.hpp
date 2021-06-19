53afd2c1ace69ba2e9605e97130234e4cd662c52
Marie Bassieux <mariebassieux@imac-de-marie.home>
2021-06-19 07:10:23 +0200

    1

--- Module_07/ex01/inc/iter.hpp
+++ Module_07/ex01/inc/iter.hpp
@@ -3,10 +3,36 @@
 #include <iostream>
 #include <string>
 
-template <typename T>
-void display(T const &f)
+class Awesome
 {
-	std::cout << "| " << f << "\t";
+public:
+	Awesome(void) : _n(42) { return; }
+	int get(void) const { return this->_n; }
+
+private:
+	int _n;
+};
+
+std::ostream &operator<<(std::ostream &o, Awesome const &rhs)
+{
+	o << rhs.get();
+	return o;
+}
+
+template <typename T>
+void print(T const &x)
+{
+	std::cout << x << std::endl;
+	return;
+}
+
+
+
+//=================================================================
+template <typename T>
+void display(T const &disp)
+{
+	std::cout << "| " << disp << "\t";
 }
 
 template <typename T>
--- Module_07/ex01/srcs/main.cpp
+++ Module_07/ex01/srcs/main.cpp
@@ -5,16 +5,21 @@ int main()
 	int a[5] = {0, 1, 2, 3, 4};
 	iter(a, 5, display);
 	std::cout << std::endl;
-	
+
 	float b[5] = {1.1, 2.2, 3.3, 4.4, 5.5};
 	iter(b, 5, display);
 	std::cout << std::endl;
-	
+
 	std::string c[5] = {"s1", "s2", "s3", "s4", "s5"};
 	iter(c, 5, display);
 	std::cout << std::endl;
 
-	
 
+//========================================
+	int tab[] = {0, 1, 2, 3, 4};
+	Awesome tab2[5];
+	iter(tab, 5, print);
+	iter(tab2, 5, print);
+	return 0;
 	return 0;
 }
