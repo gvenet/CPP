3849d7ad69946d04ed7220343f42efbaa6c04e76
Marie Bassieux <mariebassieux@imac-de-marie.home>
2021-06-19 11:45:26 +0200

    1

--- Module_07/ex02/inc/array.hpp
+++ Module_07/ex02/inc/array.hpp
@@ -2,13 +2,83 @@
 #define ARRAY_HPP
 #include <iostream>
 #include <string>
+#include <exception>
 
 template <typename T>
 class Array
 {
 private:
+	size_t _size;
+	T *_tptr;
 
 public:
+	Array() : _size(0), _tptr(nullptr)
+	{
+	}
+
+	Array(size_t n) : _size(n)
+	{
+		_tptr = new T[n];
+	}
+
+	Array(Array<T> const &cpy)
+	{
+		if (cpy._size > 0)
+		{
+			_size = cpy._size;
+			_tptr = new T[cpy._size];
+			for (size_t i = 0; i < _size; i++)
+				_tptr[i] = cpy._tptr[i];
+		}
+	}
+
+	virtual ~Array()
+	{
+		delete[] _tptr;
+	}
+
+	Array &operator=(Array<T> const &op)
+	{
+		_size = op._size;
+		if (_size > 0)
+		{
+			delete[] _tptr;
+			_tptr = new T[op._size];
+			for (size_t i = 0; i < _size; i++)
+				_tptr[i] = op._tptr[i];
+		}
+		return *this;
+	}
+
+	T &operator[](size_t i)
+	{
+		if (i >= _size)
+			throw limitArrayException();
+		return _tptr[i];
+	}
+
+	void display(void)
+	{
+		std::cout << "| ";
+		for (size_t i = 0; i < _size; i++)
+			std::cout  << _tptr[i] << " | ";
+	}
+
+	size_t size(void) const
+	{
+		return _size;
+	}
+
+	T *address(void) const
+	{
+		return _tptr;
+	}
+
+	class limitArrayException : public std::exception
+	{
+		virtual const char *what() const throw() { return "the array index is out of range"; }
+	};
+
 
 };
 
--- Module_07/ex02/srcs/main.cpp
+++ Module_07/ex02/srcs/main.cpp
@@ -1,9 +1,61 @@
 #include "../inc/array.hpp"
+#include "string"
 
-int main()
+int main(void)
 {
-	
+	Array<int> a;
 
+	Array<int> b(5);
+	a = b;
 
-	return 0;
+	a.display();
+	std::cout << std::endl;
+	for (size_t i = 0; i < a.size(); i++)
+		a[i] = i;
+	a.display();
+	std::cout << std::endl;
+	b.display();
+	std::cout << std::endl
+			  << std::endl;
+
+	Array<float> c(5);
+	for (size_t i = 0; i < c.size(); i++)
+		c[i] = 4.2;
+	c.display();
+	std::cout << std::endl
+			  << std::endl;
+
+	Array<std::string> d(5);
+	std::string stab[5] = {"s1", "s2", "s3", "s4", "s5"};
+	for (size_t i = 0; i < d.size(); i++)
+		d[i] = stab[i];
+	d.display();
+	std::cout << std::endl;
+
+	Array<std::string> e = d;
+	for (size_t i = 0; i < 5; i++)
+		e[i] = "ST";
+	e.display();
+	std::cout << std::endl
+			  << std::endl;
+
+	try
+	{
+		b[-1] = 2;
+	}
+	catch (const std::exception &e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+
+	try
+	{
+		b[999] = 2;
+	}
+	catch (const std::exception &e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+
+	return (0);
 }
