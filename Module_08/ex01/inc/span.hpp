bb3058f13e75b46311421a8bd19f5e2ce4a0317e
Marie Bassieux <mariebassieux@imac-de-marie.home>
2021-06-20 22:55:59 +0200

    1

--- Module_08/ex01/inc/span.hpp
+++ Module_08/ex01/inc/span.hpp
@@ -2,8 +2,29 @@
 #define SPAN_HPP
 
 #include <algorithm>
-#include <vector>
 #include <iostream>
-#include <list>
+#include <exception>
+
+class Span
+{
+private:
+	size_t _size;
+	int *_t;
+	size_t _fill;
+
+public:
+	Span(size_t size);
+	~Span();
+	void addNumber(const int nb);
+	size_t size(void) const;
+	int &operator[](size_t index);
+	int &shortestSpan(void);
+	int &longestSpan(void);
+	void display(void) const;
+	class SpanException : public std::exception
+	{
+		virtual const char *what() const throw() { return "SpanException : already filled"; }
+	};
+};
 
 #endif
--- Module_08/ex01/srcs/span.cpp
+++ Module_08/ex01/srcs/span.cpp
@@ -1,2 +1,43 @@
 #include "../inc/span.hpp"
 
+Span::Span(size_t size) : _size(size), _fill(0)
+{
+	_t = new int[_size];
+}
+
+Span::~Span()
+{
+	delete[] _t;
+}
+
+void Span::addNumber(const int nb)
+{
+	if (_fill == _size)
+		throw SpanException();
+	_t[_fill] = nb;
+	_fill++;
+}
+
+size_t Span::size(void) const
+{
+	return _size;
+}
+
+int &Span::operator[](size_t i)
+{
+	return _t[i];
+}
+
+void Span::display(void) const
+{
+	for (size_t i = 0; i < _size; i++)
+		std::cout << _t[i] << std::endl;
+}
+
+int &Span::shortestSpan(void)
+{
+}
+
+int &longestSpan(void)
+{
+}
