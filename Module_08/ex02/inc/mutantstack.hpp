18a6c028746d46b53b79d08524b67dc33c877c49
Guillaume Venet <gvenet@z3r6p3.42lyon.fr>
2021-06-21 15:14:40 +0200

    1

--- Module_08/ex02/inc/mutantstack.hpp
+++ Module_08/ex02/inc/mutantstack.hpp
@@ -1,8 +1,18 @@
 #ifndef MUTANTSTACK_HPP
 #define MUTANTSTACK_HPP
 #include <iostream>
+#include <stack>
+#include <vector>
+
+template <typename T>
+class MutantStack : public Stack<T>
+{
+	private:
 
 
+	public:
+		
+};
 
 
 #endif
