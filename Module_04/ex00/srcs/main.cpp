c6c83115145cadd94db6b67fa2ddce66833e52f5
Marie Bassieux <mariebassieux@imac-de-marie.home>
2021-06-25 06:32:01 +0200

    1

--- Module_04/ex00/srcs/main.cpp
+++ Module_04/ex00/srcs/main.cpp
@@ -10,21 +10,22 @@ int main()
 	std::cout << robert << jim << joe;
 	robert.polymorph(jim);
 	robert.polymorph(joe);
-	std::cout << std::endl;
+	std::cout << std::endl
+			  << std::endl;
 
+	std::cout << "==========CONSTRUC==========" << std::endl;
 	Sorcerer S(robert);
 	Victim V(jim);
 	Peon P(joe);
-	std::cout << "============CPY=============" << std::endl << S << V << P;
+	std::cout << "============CPY=============" << std::endl
+			  << S << V << P;
 
 	S.polymorph(V);
 	V = P;
 	S.polymorph(V);
 	S.polymorph(P);
 
-	std::cout << "===========DESTRUCT=========" <<std::endl;
-
-
+	std::cout << "===========DESTRUCT=========" << std::endl;
 
 	return 0;
 }
