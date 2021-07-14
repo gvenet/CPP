7193999016f1e90847bf8e2c068d1ef0adc8bec1
Guillaume Venet <gvenet@z4r9p7.42lyon.fr>
2021-07-14 11:22:31 +0200

    1

--- Module_07/ex00/srcs/main.cpp
+++ Module_07/ex00/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: gvenet <gvenet@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/13 08:33:31 by gvenet            #+#    #+#             */
-/*   Updated: 2021/07/13 08:33:31 by gvenet           ###   ########.fr       */
+/*   Updated: 2021/07/14 11:21:51 by gvenet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,23 @@
 
 int main(void)
 {
+	std::cout << "=========int==========\n"; 
 	int a = 2;
 	int b = 3;
 	::swap(a, b);
 	std::cout << "a = " << a << ", b = " << b << std::endl;
 	std::cout << "min( a, b ) = " << ::min(a, b) << std::endl;
-	std::cout << "max( a, b ) = " << ::max(a, b) << std::endl;
+	std::cout << "max( a, b ) = " << ::max(a, b) << std::endl << std::endl;
 
+	std::cout << "=========string==========\n"; 
 	std::string c = "chaine1";
 	std::string d = "chaine2";
 	::swap(c, d);
 	std::cout << "c = " << c << ", d = " << d << std::endl;
 	std::cout << "min( c, d ) = " << ::min(c, d) << std::endl;
-	std::cout << "max( c, d ) = " << ::max(c, d) << std::endl;
+	std::cout << "max( c, d ) = " << ::max(c, d) << std::endl << std::endl;
 
+	std::cout << "=========awesome==========\n"; 
 	Awesome e(50);
 	Awesome f(25);
 	::swap(e, f);
