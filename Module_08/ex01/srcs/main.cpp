b64056f3f3ec53ba23038ec2fba89791dc9b2eb2
Guillaume Venet <gvenet@z4r9p7.42lyon.fr>
2021-07-14 18:57:42 +0200

    1

--- Module_08/ex01/srcs/main.cpp
+++ Module_08/ex01/srcs/main.cpp
@@ -6,11 +6,19 @@
 /*   By: gvenet <gvenet@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/14 08:58:46 by gvenet            #+#    #+#             */
-/*   Updated: 2021/07/14 18:38:06 by gvenet           ###   ########.fr       */
+/*   Updated: 2021/07/14 18:55:50 by gvenet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/span.hpp"
+
+void disp(Span &s)
+{
+	std::cout << "nbFilled :\t" << s.getNbFill() << std::endl
+			  << "shortest :\t" << s.shortestSpan() << std::endl
+			  << "longest :\t" << s.longestSpan() << std::endl
+			  << std::endl;
+}
 
 int main(void)
 {
@@ -19,12 +27,12 @@ int main(void)
 	sp.addNumber(77);
 	sp.addNumber(10);
 	sp.addNumber(100);
-	sp.addNumber(52);
-	sp.addNumber(162);
+	sp.addNumber(90);
+	sp.addNumber(200);
 
 	try
 	{
-		std::cout << sp << sp.shortestSpan() << std::endl;
+		std::cout << sp << "shortest :\t" << sp.shortestSpan() << std::endl;
 	}
 	catch (const std::exception &e)
 	{
@@ -33,7 +41,7 @@ int main(void)
 
 	try
 	{
-		std::cout << sp.longestSpan() << std::endl;
+		std::cout << "longest :\t" << sp.longestSpan() << std::endl;
 	}
 	catch (const std::exception &e)
 	{
@@ -43,15 +51,13 @@ int main(void)
 	std::cout << "\n*****************************************\n";
 	try
 	{
-		sp.addNumber(5);
-		sp.addNumber(15);
-		std::cout << sp << sp.shortestSpan() << std::endl
-				  << sp.longestSpan() << std::endl
-				  << std::endl;
+		sp.addNumber(0);
+		sp.addNumber(20);
+		std::cout << sp;
+		disp(sp);
 		sp.addNumber(230);
-		std::cout << sp << sp.shortestSpan() << std::endl
-				  << sp.longestSpan() << std::endl
-				  << std::endl;
+		std::cout << sp;
+		disp(sp);
 		sp.addNumber(1);
 		sp.addNumber(17);
 		sp.addNumber(11);
@@ -62,15 +68,12 @@ int main(void)
 	}
 	std::cout << "\n*****************************************\n";
 	std::cout << sp;
-	std::cout << sp.shortestSpan() << std::endl;
-	std::cout << sp.longestSpan() << std::endl;
-	std::cout << std::endl;
+	disp(sp);
 
+	std::cout << "\n*****************************************\n";
 	Span spp(10000);
 	spp.autofill();
-	std::cout << "shortest\t" << spp.shortestSpan() << std::endl;
-	std::cout << "longest \t" << spp.longestSpan() << std::endl;
-	std::cout << std::endl;
+	disp(spp);
 
 	return 0;
 }
